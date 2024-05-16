43b5c4e0ec90d5644a08b3ebb36a287820856e9f
Yannick-N <yannick.napoleon@epitech.eu>
2024-05-16 15:13:46 +0200

    [HOMEF-26] : add Angular Material

--- ng-home-pedia/src/app/app.module.ts
+++ ng-home-pedia/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, NavbarComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatButtonModule, MatIconModule, MatToolbarModule],
   providers: [],
   bootstrap: [AppComponent],
 })
